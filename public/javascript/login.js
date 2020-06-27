b791c2062c4c0b6e4ed778d76094ee9cdcdedd67
Chris Acedo <tempeacedo@yahoo.com>
2020-06-27 16:07:43 -0700

    Added Sign-Up Page

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,31 +1,4 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-    // check the response status
-    if (response.ok) {
-        console.log('success');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
-
-
-  // will throw error until connected to the backend
+// will throw error until connected to the backend
   async function loginFormHandler(event) {
     event.preventDefault();
   
@@ -52,5 +25,3 @@ async function signupFormHandler(event) {
   }
 
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler)
-
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
